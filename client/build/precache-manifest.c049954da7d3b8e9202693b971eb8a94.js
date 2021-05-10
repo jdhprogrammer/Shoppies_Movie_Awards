5a52791c1e34b2178ae881b628bf432f1b948826
David Harris <jdh.programmer@gmail.com>
2021-05-09 22:44:07 -0500

    adding server.js to heroku

---
+++ client/build/precache-manifest.c049954da7d3b8e9202693b971eb8a94.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7d562033904efe2a91fcf9dd266bbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c72499da124bc51fb414",
+    "url": "/static/css/main.38d7648b.chunk.css"
+  },
+  {
+    "revision": "f5146fa3f66a04c514da",
+    "url": "/static/js/2.6ecacc76.chunk.js"
+  },
+  {
+    "revision": "af2e36f802376a46fc01e419f05b1bc7",
+    "url": "/static/js/2.6ecacc76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c72499da124bc51fb414",
+    "url": "/static/js/main.ab7f404b.chunk.js"
+  },
+  {
+    "revision": "a844db0cd49048bb6851",
+    "url": "/static/js/runtime-main.8c2761b1.js"
+  },
+  {
+    "revision": "0ef1e44e5924c1e3fad234ca259a339f",
+    "url": "/static/media/movie_award.0ef1e44e.png"
+  },
+  {
+    "revision": "648caa76780431b256f9796de6804626",
+    "url": "/static/media/movie_search.648caa76.png"
+  },
+  {
+    "revision": "ea2990a4f1c330b6b1eb086e69f93929",
+    "url": "/static/media/omdb_shoppies_banner.ea2990a4.jpg"
+  }
+]);
---
+++ scripts/seedDB.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+
+// This file empties the Movies collection and inserts the movies below
+mongoose.connect(process.env.MONGODB_URI || `mongodb+srv://jdhprogrammer:Spoons081410!@cluster0.vjufe.mongodb.net/shoppiesDB?retryWrites=true&w=majority`);
+
+const movieSeed = [
+    {
+        title: "Guardians of the Galaxy 2",
+        year: "2014",
+        id: "tt2015381",
+        type: "movie",
+        poster: "https://m.media-amazon.com/images/M/MV5BMTAwMjU5OTgxNjZeQTJeQWpwZ15BbWU4MDUxNDYxODEx._V1_SX300.jpg",
+        date: new Date(Date.now())
+    }
+];
+
+db.Movie.remove({})
+    .then(() => db.Movie.collection.insertMany(movieSeed))
+    .then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
+
+  // `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vjufe.mongodb.net/shoppiesDB?retryWrites=true&w=majority`;
